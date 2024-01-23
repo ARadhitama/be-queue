6c2fc2f80e6a026270cf47ce98831c25e12e9ed7
ariasatyaa <ariasatyaa@garena.co.id>
2024-01-23 06:24:39 +0000

    feat: list all endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,18 +1,20 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from api import views
 from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
+    path("test/", csrf_exempt(views.TestView.as_view())),
     path("info/", csrf_exempt(views.Info.as_view())),
+    path("province/", csrf_exempt(views.ProvinceView.as_view())),
+    path("city/", csrf_exempt(views.CityView.as_view())),
     path("category/", csrf_exempt(views.CategoryView.as_view())),
     path("services/", csrf_exempt(views.Services.as_view())),
     path("service_details/", csrf_exempt(views.ServiceDetailsView.as_view())),
     path("check_queue/", csrf_exempt(views.CheckQueueView.as_view())),
-    path("queue/", csrf_exempt(views.QueueUserView.as_view())),
-    path("history/", csrf_exempt(views.QueueUserView.as_view())),
-    path("cancel/", csrf_exempt(views.CheckQueueView.as_view())),
+    path("queue/", csrf_exempt(views.Queue.as_view())),
+    path("history/", csrf_exempt(views.HistoryView.as_view())),
+    path("cancel/", csrf_exempt(views.CancelQueueView.as_view())),
     path("create_services/", csrf_exempt(views.CheckQueueView.as_view())),
     path("owned_services/", csrf_exempt(views.CheckQueueView.as_view())),
     path("edit_services/", csrf_exempt(views.CheckQueueView.as_view())),
