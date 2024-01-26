4ac545e5c16c7e2f05b3e2b6a146b3aecc003ea8
ariasatyaa <ariasatyaa@garena.co.id>
2024-01-26 13:21:37 +0000

    add check owner decorator

--- api/decorators.py
+++ api/decorators.py
@@ -1,17 +1,27 @@
 from functools import wraps
 
 from api.db_manager import get_session, json_response_error
-from api.global_var import NOT_LOGGED_IN
+from api.global_var import NOT_LOGGED_IN, NOT_OWNER
 
 
 def api_check_login(f):
     @wraps(f)
     def wrapper(request, *args, **kwargs):
         try:
-            user_data = get_session(request.headers.get("authorization", None))
+            session = get_session(request.headers.get("authorization", None))
         except:
             return json_response_error(NOT_LOGGED_IN)
 
-        return f(request, user_data, *args, **kwargs)
+        return f(request, session, *args, **kwargs)
+
+    return wrapper
+
+
+def api_check_owner(f):
+    @wraps(f)
+    def wrapper(request, user_data, *args, **kwargs):
+        if not user_data["user_type"]:
+            return json_response_error(NOT_OWNER)
+        return f(request, *args, **kwargs)
 
     return wrapper
