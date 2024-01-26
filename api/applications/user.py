ee85d5c9b25c3368deaae022975c36cc722f9a0b
ariasatyaa <ariasatyaa@garena.co.id>
2024-01-26 13:35:08 +0000

    init user app

--- api/applications/user.py
+++ api/applications/user.py
@@ -1,11 +1,15 @@
 from .base import Application, BaseError
 
+from api import models as M
+from api.applications.static import StaticApp
 from oauth.models import UserProfile
+
 
 class UserApp(Application):
     def __init__(self, id):
-        self.__id = id
-    
+        self.id = id
+        self.static = StaticApp()
+
     def get_data_obj(self):
-        return UserProfile.objects.filter(id=self.__id).first()
-    
+        return UserProfile.objects.filter(id=self.id).first()
+
